27cb8660e558cb1d6b7988e8c771f2ff7e17d891
Niels Drost <nielsdrost7@gmail.com>
2017-02-05 13:13:39 +0100

    Calendar Small Fixes

--- Modules/Calendar/Resources/views/calendar.blade.php
+++ Modules/Calendar/Resources/views/calendar.blade.php
@@ -6,7 +6,9 @@
 
 @section('scripts')
     <script>
-        $(document).ready(function(){
+        $(document).ready(function () {
+
+
             $('#calendar-myperfectcalendar').fullCalendar({
                 //weekends: false // will hide Saturdays and Sundays
                 header: {
@@ -18,7 +20,7 @@
 
                 selectable: true,
                 selectHelper: true,
-                select: function(start, end) {
+                select: function (start, end) {
                     var title = prompt('Event Title:');
                     var eventData;
                     if (title) {
@@ -32,64 +34,62 @@
                     $('#calendar').fullCalendar('unselect');
                 },
                 editable: true,
+                droppable: true,
                 eventLimit: true, // allow "more" link when too many events
 
                 navLinks: true, // can click day/week names to navigate views
                 businessHours: true, // display business hours
 
                 nowIndicator: true,
-                events: [
-                    {
-                        title: 'Business Lunch',
-                        start: '2017-02-08T13:00:00',
-                        constraint: 'businessHours'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2017-02-13T11:00:00',
-                        constraint: 'availableForMeeting', // defined below
-                        color: '#257e4a'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2017-02-18',
-                        end: '2017-02-20'
-                    },
-                    {
-                        title: 'Party',
-                        start: '2017-02-27T20:00:00'
-                    },
 
-                    // areas where "Meeting" must be dropped
+                eventSources: [
+                    // your event source
+                    // your event source
                     {
-                        id: 'availableForMeeting',
-                        start: '2017-02-21T10:00:00',
-                        end: '2017-02-21T16:00:00',
-                        rendering: 'background'
-                    },
-                    {
-                        id: 'availableForMeeting',
-                        start: '2017-02-23T10:00:00',
-                        end: '2017-02-23T16:00:00',
-                        rendering: 'background'
-                    },
-
-                    // red areas where no events can be dropped
-                    {
-                        start: '2017-02-24',
-                        end: '2016-02-28',
-                        overlap: false,
-                        rendering: 'background',
-                        color: '#ff9f89'
-                    },
-                    {
-                        start: '2017-03-06',
-                        end: '2017-03-08',
-                        overlap: false,
-                        rendering: 'background',
-                        color: '#ff9f89'
+                        events: function (start, end, timezone, callback) {
+                            jQuery.ajax({
+                                url: 'schedule.php/load',
+                                type: 'POST',
+                                dataType: 'json',
+                                data: {
+                                    start: start.format(),
+                                    end: end.format()
+                                },
+                                success: function (doc) {
+                                    var events = [];
+                                    if (!!doc.result) {
+                                        $.map(doc.result, function (r) {
+                                            events.push({
+                                                id: r.id,
+                                                title: r.title,
+                                                start: r.date_start,
+                                                end: r.date_end
+                                            });
+                                        });
+                                    }
+                                    callback(events);
+                                }
+                            });
+                        },
+                        color: 'yellow',   // an option!
+                        textColor: 'black' // an option!
                     }
-                ]
+                    // any other sources...
+                ],
+
+                /*events: {
+                 url: '/myfeed.php',
+                 type: 'POST',
+                 data: {
+                 custom_param1: 'something',
+                 custom_param2: 'somethingelse'
+                 },
+                 error: function() {
+                 alert('there was an error while fetching events!');
+                 },
+                 color: 'yellow',   // a non-ajax option
+                 textColor: 'black' // a non-ajax option
+                 }*/
             });
         });
     </script>
